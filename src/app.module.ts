da712790f59431274c6237af1c14ec99c4dcf061
AnhHo <hotrunganhht1912>
2022-07-22 00:44:06 +0700

    feat(): add notification

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,38 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { UserModule } from './user/user.module'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { join } from 'path'
-import { StoriesModule } from 'stories/stories.module'
-import { ChapperModule } from 'chapper/chapper.module'
-import { DiscussModule } from 'discuss/discuss.module'
-import { CommentModule } from 'comment/comment.module'
+import {Module} from '@nestjs/common'
+import {AppController} from './app.controller'
+import {AppService} from './app.service'
+import {UserModule} from './user/user.module'
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {join} from 'path'
+import {StoriesModule} from 'stories/stories.module'
+import {ChapperModule} from 'chapper/chapper.module'
+import {DiscussModule} from 'discuss/discuss.module'
+import {CommentModule} from 'comment/comment.module'
+import {NotificationModule} from "./notification/notification.module";
 
 @Module({
-  imports: [
-    UserModule,
-    StoriesModule,
-    ChapperModule,
-    DiscussModule,
-    CommentModule,
-    TypeOrmModule.forRoot(
-        {
-          url: 'mongodb+srv://huynhduc:duc123456789@cluster0.pvxto.mongodb.net/doctruyen',
-          // url: 'mongodb://localhost:27017/eventmanage',
-          type: "mongodb",
-          entities: [join(__dirname, '**/**.entity{.ts,.js}')],
-          synchronize: true,
-          useNewUrlParser: true,
-          logging: true,
-          useUnifiedTopology: true
-        }
-    ),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        UserModule,
+        StoriesModule,
+        ChapperModule,
+        DiscussModule,
+        CommentModule,
+        NotificationModule,
+        TypeOrmModule.forRoot(
+            {
+                url: 'mongodb+srv://huynhduc:duc123456789@cluster0.pvxto.mongodb.net/doctruyen',
+                // url: 'mongodb://localhost:27017/eventmanage',
+                type: "mongodb",
+                entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+                synchronize: true,
+                useNewUrlParser: true,
+                logging: true,
+                useUnifiedTopology: true
+            }
+        ),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {
 }
